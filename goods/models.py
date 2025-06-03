1b1683c9c5d970c82b790c25a56f07293f79d290
saythe0 <vadimsaythe@gmail.com>
2025-06-03 11:23:33 +0400

    9 урок

--- goods/models.py
+++ goods/models.py
@@ -8,7 +8,7 @@ class Products(models.Model):
     description = models.TextField(blank=True, null=True, verbose_name='Описание')
     image = models.ImageField(upload_to='goods_images', blank=True, null=True, verbose_name='Изображение')
     price = models.DecimalField(default=0.00, decimal_places=2, max_digits=8, verbose_name='Цена')
-    discount = models.DecimalField(default=0.00, decimal_places=2, max_digits=8, verbose_name='Цена со скидкой')
+    discount = models.DecimalField(default=0.00, decimal_places=2, max_digits=8, verbose_name='Скидка в процентах')
 
     class Meta:
         db_table = 'products'
@@ -17,3 +17,9 @@ class Products(models.Model):
 
     def __str__(self):
         return self.name
+
+    def sell_price(self):
+        if self.discount:
+            return round(self.price - self.price * self.discount / 100, 2)
+
+        return self.price
