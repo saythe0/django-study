7d674a649856d1b8590883190eb7eeb0ae88b544
saythe0 <vadimsaythe@gmail.com>
2025-06-03 10:26:41 +0400

    5 урок

--- app/urls.py
+++ app/urls.py
@@ -17,9 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from main import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
+    path('catalog/', include('goods.urls')),
 ]
---
+++ goods/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from goods import views
+
+urlpatterns = [
+    path('', views.catalog, name='catalog'),
+    path('product/', views.product, name='product'),
+]
