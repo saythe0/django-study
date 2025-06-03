81c7fdd8dab3ba69fb1b0c368a093c6a260f8f6e
saythe0 <vadimsaythe@gmail.com>
2025-06-03 10:33:29 +0400

    6 урок

--- app/urls.py
+++ app/urls.py
@@ -21,5 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
-    path('catalog/', include('goods.urls')),
+    path('catalog/', include('goods.urls', namespace='catalog')),
 ]
--- goods/urls.py
+++ goods/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from goods import views
 
+app_name = 'goods'
+
 urlpatterns = [
     path('', views.catalog, name='catalog'),
     path('product/', views.product, name='product'),
